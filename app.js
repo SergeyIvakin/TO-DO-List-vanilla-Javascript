31176073a10030bb42cc237935aaae9df493a8f5
SergeyIvakin <kolobin87@yandex.ru>
2021-06-10 21:56:08 +0300

    add local storage

--- app.js
+++ app.js
@@ -1,55 +1,117 @@
 const btnAddTask = document.querySelector('#btn-add-task'),
-              inputTask = document.querySelector('#input-task'),
-              toDoBox = document.querySelector('#to-do-box'),
-              btn_2 = document.querySelector('.btn-2'),
-              imgDelete = document.querySelector('img'),
-              arrToDoList = [];
+      inputTask = document.querySelector('#input-task'),
+      toDoBox = document.querySelector('.to-do-box'),
+      ulTask = document.querySelector('.ul-task'),
+      //arrayKeyLocalStorage = [],
+      btn_2 = document.querySelector('.btn-2');
 
-        btnAddTask.addEventListener('click', addTaskToDoBox);
+btnAddTask.addEventListener('click', addTaskToDoBox);
+btnAddTask.addEventListener('click', deleteInputTask);
 
-        function addTaskToDoBox() {
-            let divTask = document.createElement('div'),
-                contentTask = document.createElement('p'),
-                imgDeleteTask = document.createElement('img'),
-                taskPerform = document.createElement('input');
-                
-            document.querySelector('#inactive-div').remove();   
-            taskPerform.setAttribute('type', 'checkbox');
-            imgDeleteTask.src = 'trash-solid.svg';
-            divTask.className = 'to-do';
-            imgDeleteTask.className = 'img-delete';
-            taskPerform.className = 'checkbox-task-done';
-            contentTask.textContent = inputTask.value.trim();
-            divTask.prepend(taskPerform);
-            divTask.prepend(imgDeleteTask);
-            divTask.append(contentTask);
-            toDoBox.append(divTask);
 
+function addTaskToDoBox() {
+
+    let li = document.createElement('li');
+
+    li.innerHTML = `<span class="delete-task">×</span><input type='checkbox' class="done-task"><label>${inputTask.value}</label>`;
+
+    ulTask.append(li);
+
+    let keyLocalStorage = Math.floor( Math.random() * 10000);
+
+    localStorage.setItem(keyLocalStorage, JSON.stringify(li.innerHTML));
+
+    btnAddTask.setAttribute('disabled', 'true');
+
+    toDoBox.addEventListener('change', taskDone);
+
+    btnDeleteTask = document.querySelectorAll('.delete-task');
+
+    for(let i = 0; i < btnDeleteTask.length; i++) {
+        
+        btnDeleteTask[i].onclick = deleteTask;
+         
+    }
+}
+
+
+function outputsContentLocalStorage() {
+
+    let arrayKeyLocalStorage = [];
+
+    for(let i = 0; i < localStorage.length; i++) {
+        
+        let key = localStorage.key(i),
+            a = localStorage.getItem(`${key}`),
+            b = JSON.parse(a),
+            li = document.createElement('li');
+
+    li.innerHTML = b;
+
+    ulTask.append(li);
+
+    arrayKeyLocalStorage[i] = localStorage.key(i);
+    }
+
+    let btnDeleteTask = document.querySelectorAll('.delete-task');
+
+    for(let i = 0; i < btnDeleteTask.length; i++) {
+        
+        btnDeleteTask[i].onclick = function (event) {
+
+           event.target.parentNode.remove();
+               
+           localStorage.removeItem(arrayKeyLocalStorage[i]); 
+
+    }    
+} 
+}
+
+outputsContentLocalStorage();
+
+
+function deleteTask(event) {
+    
+    event.target.parentNode.remove();//parentNode - обращение к родительскому элементу
+    //outputsContentLocalStorage();
+    
+}
+
+
+function taskDone() {
+
+    let doneTask = document.querySelectorAll('.done-task');
+
+    for(let i = 0; i < doneTask.length; i++) {
+        if(doneTask[i].checked) {
+            doneTask[i].nextSibling.style.textDecoration = 'line-through';//обращаемся к соседнему от doneTask[i] элементу
         }
+        else {
+            doneTask[i].nextSibling.style.textDecoration = 'none';
+        }  
+    }
+} 
 
-        btn_2.onclick = function () {
-            //document.querySelector('.to-do').remove();//удаляет див со всем содержимым
-            //document.querySelector('.to-do').style.textDecoration = 'line-through';//зачеркивает текст
-            document.querySelector('.to-do').classList.add('task-done');//зачеркивает текст
-        }
 
-        let donetask = document.querySelectorAll('.checkbox-task-done');
-        console.log(donetask);
-        for(let i = 0; i < donetask.length; i++) {
-            donetask[i].onclick = function() {
-                console.log(8);
-                document.querySelector('.to-do').classList.add('task-done');
-            }
-        }
+function deleteInputTask() {
 
-        imgDelete.addEventListener('click', f1);
+    inputTask.value = '';
+}
 
-        function f1() {
-            document.querySelector('.to-do').remove();
+inputTask.addEventListener('input', () => {
+    
+    let input = inputTask.value.trim();
+    
+    if(input != '') {
+        btnAddTask.removeAttribute('disabled');
+    }
+    else {
+        btnAddTask.setAttribute('disabled', 'true'); 
+    }
+})
+
             
-        }
        
         
       
 
-        
---
+++ primer.js
@@ -0,0 +1,49 @@
+const btnAddTask = document.querySelector('#btn-add-task'),
+      inputTask = document.querySelector('#input-task'),
+      toDoBox = document.querySelector('.to-do-box'),
+      ulTask = document.querySelector('.ul-task'),
+      btn_2 = document.querySelector('.btn-2');
+
+        btnAddTask.addEventListener('click', addTaskToDoBox);
+
+        function addTaskToDoBox() {
+            let divTask = document.createElement('div'),
+                contentTask = document.createElement('p'),
+                imgDeleteTask = document.createElement('img'),
+                taskPerform = document.createElement('input');
+                
+            document.querySelector('#inactive-div').remove();   
+            taskPerform.setAttribute('type', 'checkbox');
+            imgDeleteTask.src = 'trash-solid.svg';
+            divTask.className = 'to-do';
+            imgDeleteTask.className = 'img-delete';
+            taskPerform.className = 'checkbox-task-done';
+            contentTask.textContent = inputTask.value.trim();
+            divTask.prepend(taskPerform);
+            divTask.prepend(imgDeleteTask);
+            divTask.append(contentTask);
+            toDoBox.append(divTask);
+
+        }
+
+        btn_2.onclick = function () {
+            //document.querySelector('.to-do').remove();//удаляет див со всем содержимым
+            //document.querySelector('.to-do').style.textDecoration = 'line-through';//зачеркивает текст
+            document.querySelector('.to-do').classList.add('task-done');//зачеркивает текст
+        }
+
+        let donetask = document.querySelectorAll('.checkbox-task-done');
+        console.log(donetask);
+        for(let i = 0; i < donetask.length; i++) {
+            donetask[i].onclick = function() {
+                console.log(8);
+                document.querySelector('.to-do').classList.add('task-done');
+            }
+        }
+
+        imgDelete.addEventListener('click', f1);
+
+        function f1() {
+            document.querySelector('.to-do').remove();
+            
+        }
